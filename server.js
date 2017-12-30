c7ae291d714f456ed5fda829c5cccbf8c803b483
littlestar-642 <avinashjasiwal642@gmail.com>
2017-12-31 02:19:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express=require('express');
+var app=express();
+const hbs=require('hbs');
+const fs=require('fs');
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+hbs.registerHelper("getYear",()=>{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamIt",(text)=>{
+    return text.toUpperCase();
+
+});
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs');
+    
+//     next();
+// });
+
+
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}:${req.method} ${req.url}`;
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log("there was an error");
+        }
+    });
+    next();
+
+});
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        title:'Home Page',
+        
+        welMsg:"Hi! You are on the home page"
+    });
+
+});
+
+
+app.get('/about',(req,res)=>{
+    res.render("about..hbs",{
+        title:'About Page',
+        year:new Date().getFullYear()
+    });
+});
+app.get('/bad',(req,res)=>{
+    res.render('about.hbs');
+});
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000")
+});
